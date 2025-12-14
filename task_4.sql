f7e7a60d9c7b207deb114e23c1d506068ccc78ab
amanuel912 <amanu.hk82@gmail.com>
2025-12-14 09:57:23 +0900

    fix: correct SQL query to select columns from the Books table in alx_book_store schema

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,4 @@
-SELECT Books
-FROM information.schema.TABLES
-where TABLE.schema = 'alx_book_store'
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND  TABLE_NAME = 'Books'
